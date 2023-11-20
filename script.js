0149d3783d996b18edd7b19fe501260042d29d30
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2023-11-21 01:35:28 +0530

    Added Readme

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 var mediaQuery = window.matchMedia("(max-width: 800px)");
+let userAgent = navigator.userAgent;
+var browserName = (function (agent) {
+  switch (true) {
+    case agent.indexOf("edge") > -1:
+      return "MS Edge";
+    case agent.indexOf("edg/") > -1:
+      return "Edge ( chromium based)";
+    case agent.indexOf("opr") > -1 && !!window.opr:
+      return "Opera";
+    case agent.indexOf("chrome") > -1 && !!window.chrome:
+      return "Chrome";
+    case agent.indexOf("trident") > -1:
+      return "MS IE";
+    case agent.indexOf("firefox") > -1:
+      return "Mozilla Firefox";
+    case agent.indexOf("safari") > -1:
+      return "Safari";
+    default:
+      return "other";
+  }
+})(window.navigator.userAgent.toLowerCase());
+if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+  var phone = true;
+} else {
+  var phone = false;
+}
+
+var medianew = window.innerWidth;
 var media2 = window.matchMedia("(min-width: 800px)");
 window.onresize = resize();
 var resizeTimeout;
@@ -90,24 +118,39 @@ if (media2.matches) {
       y.style.width = "100%";
       y.style.marginLeft = "0%";
       z.style.opacity = "0";
-
+      y.style.filter = "blur(0px)";
       x.style.transitionDelay = "0.4s";
       z.style.transition = "all 0.3s";
       x.style.transition = "all 0.8s";
       y.style.transition = "all 0.8s";
+
       return (a = 0);
     } else {
-      y.style.width = "80%";
-      y.style.marginLeft = "20%";
-      x.style.width = "20%";
-      x.style.visibility = "visible";
-      z.style.visibility = "visible";
-      x.style.transition = "all 0.8s";
-      z.style.opacity = "1";
-      z.style.transition = "all 0.3s";
-      z.style.transitionDelay = "0.4s";
-      y.style.transition = "all 0.8s";
-      return (a = 1);
+      if (browserName === "Safari" && phone === true) {
+        x.style.width = "60%";
+        x.style.visibility = "visible";
+        z.style.visibility = "visible";
+        y.style.filter = "blur(10px)";
+        x.style.transition = "all 0.8s";
+        z.style.opacity = "1";
+        z.style.transition = "all 0.3s";
+        z.style.transitionDelay = "0.4s";
+        y.style.transition = "all 0.8s";
+
+        return (a = 1);
+      } else {
+        y.style.width = "80%";
+        y.style.marginLeft = "20%";
+        x.style.width = "20%";
+        x.style.visibility = "visible";
+        z.style.visibility = "visible";
+        x.style.transition = "all 0.8s";
+        z.style.opacity = "1";
+        z.style.transition = "all 0.3s";
+        z.style.transitionDelay = "0.4s";
+        y.style.transition = "all 0.8s";
+        return (a = 1);
+      }
     }
   }
 }
